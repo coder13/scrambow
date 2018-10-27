bc331d261b37c0f6bb379460349bd05801f16f03
Owstin <owstingreen@gmail.com>
2018-10-26 23:34:55 -0500

    update packages

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const UglifyJsPlugin  = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+  entry: {
+    scrambow: './src/scrambow.ts',
+    cli: './src/cli.ts'
+  },
+  target: 'node',
+  mode: 'none',
+  plugins: [
+    new UglifyJsPlugin()
+  ],
+  resolve: {
+    extensions: [".ts", ".js"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/
+      }
+    ]
+  },
+  output: {
+    filename: '[name].js',
+    library: 'scrambow',
+    libraryTarget: 'umd'
+  }
+};
